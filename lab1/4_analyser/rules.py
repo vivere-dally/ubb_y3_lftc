26d70813acae520b92a1332239229a7dc2372f72
daLLyw0w <stefanbuciujr@gmail.com>
2020-10-17 15:51:04 +0300

    Added more rules, changed the mlp a bit

--- lab1/4_analyser/rules.py
+++ lab1/4_analyser/rules.py
@@ -240,3 +240,273 @@ class Param(Rule):
 
         results.extend([start(), start.inc])
         return results
+
+
+class Read(Rule):
+    """
+        This class checks the given line from the given index for PowerShell Read-Host.
+    """
+
+    def __init__(self, id: Rule, declaration: Rule):
+        """
+        Args:
+            id (Rule): Rule that checks if a pattern is a PowerShell ID.
+            declaration (Rule): Rule that checks if a pattern is a PowerShell Declaration.
+        """
+        self.__id = id
+        self.__declaration = declaration
+
+    def check(self, line: str, start: utils.MutableInt) -> []:
+        if super(Param, self).check(line, start):
+            return []
+
+        results = self.__declaration.check(line, start)
+        if 0 == len(results):
+            results = self.__id.check(line, start)
+            if 0 == len(results):
+                return []
+
+        self._strip_until(line, start)  # CASE: <DECLARATION> | <ID>    =
+        if '=' != line[start()]:
+            return results
+
+        results.extend([start(), start.inc])
+        self._strip_until(line, start)  # CASE: ...    Read-Host
+        if "Read-Host" != line[start(): start() + 9]:
+            return results
+
+        results.extend([start(), start(start() + 9)])
+        self._strip_until(line, start)  # CASE: Read-Host     ;
+        if ';' == line[start()]:
+            results.append([start(), start.inc])
+            return results
+
+        return results
+
+
+class Write(Rule):
+    """
+        This class checks the given line from the given index for PowerShell Write-Host.
+    """
+
+    def __init__(self, id: Rule, const: Rule):
+        """
+        Args:
+            id (Rule): Rule that checks if a pattern is a PowerShell ID.
+            const (Rule): Rule that checks if a pattern is a PowerShell Const.
+        """
+        self.__id = id
+        self.__const = const
+
+    def check(self, line: str, start: utils.MutableInt) -> []:
+        if super(Param, self).check(line, start):
+            return []
+
+        results = []
+        if "Write-Host " != line[start(): start() + 11]:
+            return []
+
+        results.extend([start(), start(start() + 11)])
+        self._strip_until(line, start)  # CASE: Write-Host     <ID>
+        results_ = self.__id.check(line, start)
+        if 0 == len(results_):
+            results_ = self.__const.check(line, start)
+            if 0 == len(results_):
+                start.inc
+                results.append(-1)
+                return results
+
+        self._strip_until(line, start)  # CASE: Write-Host <ID>    ;
+        if ';' == line[start()]:
+            results.append([start(), start.inc])
+            return results
+
+        return results
+
+
+class Condition(Rule):
+    """
+        This class checks the given line from the given index for PowerShell Condition.
+    """
+
+    def __init__(self, id: Rule, const: Rule, equality_operators: List):
+        """
+        Args:
+            id (Rule): Rule that checks if a pattern is a PowerShell ID.
+            const (Rule): Rule that checks if a pattern is a PowerShell Const.
+            equality_operators (List): List of PowerShell equality operators.
+        """
+        self.__id = id
+        self.__const = const
+        self.__equality_operators = equality_operators
+
+    def check(self, line: str, start: utils.MutableInt) -> []:
+        if super(Param, self).check(line, start):
+            return []
+
+        results = self.__id.check(line, start)
+        if 0 == len(results):
+            results = self.__const.check(line, start)
+            if 0 == len(results):
+                return []
+
+        self._strip_until(line, start)  # CASE: ID | CONST    <EQ_OP>
+        eq_op_found = False
+        for eq_op in self.__equality_operators:
+            if eq_op_found:
+                break
+
+            if eq_op == line[start(), start() + len(eq_op)]:
+                eq_op_found = True
+                results.extend([start(), start(start() + len(eq_op))])
+
+        if not eq_op_found:
+            return results
+
+        self._strip_until(line, start)  # CASE: <EQ_OP>    ID | CONST
+        results_ = self.__id.check(line, start)
+        if 0 == len(id):
+            results_ = self.__const.check(line, start)
+            if 0 == len(results_):
+                start.inc
+                results.add(-1)
+                return results
+
+        results.extend(results_)
+        return results
+
+
+class CompoundCondition(Rule):
+    """
+        This class checks the given line from the given index for PowerShell Compound Condition.
+    """
+
+    def __init__(self, condition: Rule, logical_operators: List):
+        """
+        Args:
+            condition (Rule): Rule that checks if a pattern is a PowerShell Condition.
+            logical_operators (List): List of PowerShell logical operators.
+        """
+        self.__id = id
+        self.__condition = condition
+        self.__logical_operators = logical_operators
+
+    def check(self, line: str, start: utils.MutableInt) -> []:
+        if super(Param, self).check(line, start):
+            return []
+
+        results = []
+        if '(' == line[start(), start() + 1]:
+            start.inc
+            results.extend(self.check(line, start))
+
+        self._strip_until(line, start)  # CASE: (    <CONDITION>
+        results.extend(self.__condition.check(line, start))
+        self._strip_until(line, start)  # CASE: <CONDITION>    )
+        while ')' == line[start(), start() + 1]:
+            results.extend([start(), start.inc])
+            self._strip_until(line, start)  # CASE: <CONDITION>    )    )
+
+        self._strip_until(line, start)  # CASE: )    <LG_OP>
+        lg_op_found = False
+        for lg_op in self.__logical_operators:
+            if lg_op_found:
+                break
+
+            if lg_op == line[start(), start() + len(lg_op)]:
+                lg_op_found = True
+                results.extend([start(), start(start() + len(lg_op))])
+
+        if not lg_op_found:
+            return results
+
+        self._strip_until(line, start)  # CASE: <LG_OP>    (
+        if '(' == line[start(), start() + 1]:
+            start.inc
+            results.extend(self.check(line, start))
+
+        return results
+
+
+class If(Rule):
+    """
+        This class checks the given line from the given index for PowerShell if statement.
+    """
+
+    def __init__(self, compoundCondition: Rule):
+        """
+        Args:
+            compoundCondition (Rule): Rule that checks if a pattern is a PowerShell Compound Condition.
+        """
+        self.__id = id
+        self.__compoundCondition = compoundCondition
+
+    def check(self, line: str, start: utils.MutableInt) -> []:
+        if super(Param, self).check(line, start):
+            return []
+
+        if "if" != line[start(), start() + 2]:
+            return []
+
+        results = [start(), start(start() + 2)]
+        self._strip_until(line, start)  # CASE: if    (
+        if '(' != line[start(), start() + 1]:
+            start.inc
+            results.append(-1)
+            return results
+
+        results.extend(self.__compoundCondition.check(line, start))
+        self._strip_until(line, start)  # CASE: <CP_CON>   )
+        if ')' == line[start(), start() + 1]:
+            results.extend([start(), start.inc])
+
+        self._strip_until(line, start)  # CASE: )    {
+        if '{' != line[start(), start() + 1]:
+            start.inc
+            results.append(-1)
+            return results
+
+        results.extend([start(), start.inc])
+        return results
+
+
+class While(Rule):
+    """
+        This class checks the given line from the given index for PowerShell while statement.
+    """
+
+    def __init__(self, compoundCondition: Rule):
+        """
+        Args:
+            compoundCondition (Rule): Rule that checks if a pattern is a PowerShell Compound Condition.
+        """
+        self.__id = id
+        self.__compoundCondition = compoundCondition
+
+    def check(self, line: str, start: utils.MutableInt) -> []:
+        if super(Param, self).check(line, start):
+            return []
+
+        if "while" != line[start(), start() + 5]:
+            return []
+
+        results = [start(), start(start() + 2)]
+        self._strip_until(line, start)  # CASE: while    (
+        if '(' != line[start(), start() + 1]:
+            start.inc
+            results.append(-1)
+            return results
+
+        results.extend(self.__compoundCondition.check(line, start))
+        self._strip_until(line, start)  # CASE: <CP_CON>   )
+        if ')' == line[start(), start() + 1]:
+            results.extend([start(), start.inc])
+
+        self._strip_until(line, start)  # CASE: )    {
+        if '{' != line[start(), start() + 1]:
+            start.inc
+            results.append(-1)
+            return results
+
+        results.extend([start(), start.inc])
+        return results
